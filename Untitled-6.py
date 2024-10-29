e81b616d70bf7871dfff54f14d3970f8f88fd4d1
ViktoriaOs <104002896+ViktoriaOs@users.noreply.github.com>
2024-10-29 09:35:36 +0100

    Add files via upload

--- Untitled-6.py
+++ Untitled-6.py
@@ -15,7 +15,7 @@ def cifry(a,b):
         if i %10==3:
             x=x+i
     return x
-print(cifry(1,30))
+#print(cifry(1,30))
 
 #napis funkxciu ktora vypise parne cisla zo zadaneho intervalu vratane jeho hranic
 def parne(a,b):
@@ -23,7 +23,7 @@ def parne(a,b):
         if i%2==0:
             print(i)
 
-parne(10,20)
+#parne(10,20)
 
 #vsetkz stvorcif cisla kde prve dvojcislie je mocninou 2
 def drticka():
@@ -34,4 +34,29 @@ def drticka():
             print(i)
         if i//100==64:
             print(i)
-drticka()
+#drticka()
+
+#cifrz tvoria vyostupnu postupnost
+def idk():
+    c=0
+    for i in range(1000,10000):
+        c1=i%10
+        c2=(i%100)//10
+        c3=(i%1000)//100
+        c4=(i%10000)//1000
+        if c4<=c3<=c2<=c1:
+            c+=1
+            print(i)
+    print(c)
+#idk()
+
+#yadas cislo povedyme 10 a scitat 10+9+8+7+6+5+4+3+2+1
+def randomvzorec(n):
+    print((1+n)*(n/2))
+#randomvzorec(100)
+
+#nadobit
+import math
+def nasobenie(n):
+    print(math.factorial(n))
+nasobenie(5)
